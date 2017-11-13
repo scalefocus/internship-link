7a0c67ec1a59618be783ed4fd9ea5ee1a211ac75
Velio Ivanov <velio.ivanov@gmail.com>
2017-11-13 17:29:27 +0200

    added the mvc project

---
+++ src/InternshipLink.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternshipLink.Web.MvcApplication" Language="C#" %>
---
+++ src/InternshipLink.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternshipLink.Web.Startup))]
+namespace InternshipLink.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
