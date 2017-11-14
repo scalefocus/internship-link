dedf87ab341c0e5291c2aa0fb65830791f9b8c7e
Velyo Ivanov <velio.ivanov@gmail.com>
2017-11-14 15:18:22 +0200

    add some model/view/contoller test content

--- src/InternshipLink.Web/Controllers/HomeController.cs
+++ src/InternshipLink.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InternshipLink.Web.Models;
 
 namespace InternshipLink.Web.Controllers
 {
@@ -10,21 +11,38 @@ namespace InternshipLink.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return new ViewResult(); //View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.Test = true;
 
-            return View();
+            var model = new AboutViewModel();
+            model.MyName = "Velio";
+
+            return View(model);
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                string name = model.Name;
+                // TODO
+            }
+
+            return View(model);
         }
     }
 }
---
+++ src/InternshipLink.Web/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InternshipLink.Web.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Името е задължително")]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public int Age { get; set; }
+
+        public bool IsAdult
+        {
+            get
+            {
+                return (Name != null) && (Email != null) && (Age < 18);
+            }
+        }
+    }
+}
