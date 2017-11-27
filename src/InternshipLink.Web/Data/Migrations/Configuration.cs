6fbc90f6e17a6464447706db00309b0f493bf9d6
gargamelco <dinkov.com@gmail.com>
2017-11-27 22:14:26 +0200

    Code improvement and rearrangement in issues #2 and #3

--- src/InternshipLink.Web/Data/Migrations/Configuration.cs
+++ src/InternshipLink.Web/Data/Migrations/Configuration.cs
@@ -1,13 +1,12 @@
 namespace InternshipLink.Web.Data.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Microsoft.AspNet.Identity;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<InternshipLink.Web.Data.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
     {
         public Configuration()
         {
@@ -15,43 +14,45 @@ namespace InternshipLink.Web.Data.Migrations
             MigrationsDirectory = @"Data\Migrations";
         }
 
-        protected override void Seed(InternshipLink.Web.Data.DataContext context)
-        { 
-            context.Roles.Add(new IdentityRole { Id = "ADMINISTRATOR", Name = "Administrator" });
-            context.SaveChanges();
-            context.Roles.Add(new IdentityRole { Id = "STUDENT", Name = "Student" });
-            context.SaveChanges();
-
-            var adminRole = context.Roles.SingleOrDefault(r => r.Name == "Administrator");
-            var studentRole = context.Roles.SingleOrDefault(r => r.Name == "Student");
-
-            var hasher = new PasswordHasher();
-
-            var admin = new ApplicationUser
+        protected override void Seed(DataContext context)
+        {
+            if (!context.Roles.Any())
             {
-                UserName = "admin@admin.com",
-                PasswordHash = hasher.HashPassword("admin"),
-                Email = "admin@admin.com",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString()
-            };
 
-            var student = new ApplicationUser
-            {
-                UserName = "student@student.com",
-                PasswordHash = hasher.HashPassword("student"),
-                Email = "student@student.com",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString()
-            };
+                context.Roles.Add(new IdentityRole { Id = "ADMINISTRATOR", Name = "Administrator" });
+                context.Roles.Add(new IdentityRole { Id = "STUDENT", Name = "Student" });
+                context.SaveChanges();
 
-            admin.Roles.Add(new IdentityUserRole { RoleId = adminRole.Id, UserId = admin.Id });
-            student.Roles.Add(new IdentityUserRole { RoleId = adminRole.Id, UserId = student.Id });
+                var adminRole = context.Roles.SingleOrDefault(r => r.Name == "Administrator");
+                var studentRole = context.Roles.SingleOrDefault(r => r.Name == "Student");
 
-            context.Users.Add(admin);
-            context.SaveChanges();
-            context.Users.Add(student);
-            context.SaveChanges();
+                var hasher = new PasswordHasher();
+
+                var admin = new ApplicationUser
+                {
+                    UserName = "admin@admin.com",
+                    PasswordHash = hasher.HashPassword("admin"),
+                    Email = "admin@admin.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                var student = new ApplicationUser
+                {
+                    UserName = "student@student.com",
+                    PasswordHash = hasher.HashPassword("student"),
+                    Email = "student@student.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                context.Users.Add(student);
+                context.Users.Add(admin);
+                context.SaveChanges();
+
+                admin.Roles.Add(new IdentityUserRole { RoleId = adminRole.Id, UserId = admin.Id });
+                student.Roles.Add(new IdentityUserRole { RoleId = adminRole.Id, UserId = student.Id });
+            }
 
             base.Seed(context);
 
